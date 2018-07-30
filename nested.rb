9c0eefd4a053b84f330b44e994a599a186786942
Jae Yong Kim <kim900180@gmail.com>
2018-07-30 22:50:29 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -83,8 +83,8 @@ def adding_matz
         }
      }
 
-		programmer_hash[:yukihiro_matsumoto]
-		programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		programmer_hash => :yukihiro_matsumoto
+
 end
 
 def changing_alan
